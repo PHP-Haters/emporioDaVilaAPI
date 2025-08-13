d5d1d4548ad3bc963483126f90efdbca00968668
gabriel-castro <gabrielcastro.developer@gmail.com>
2025-08-13 20:49:49 -0300

    first commit

---
+++ src/main/java/app/emporioDaVila/EmporioDaVilaApplication.java
@@ -0,0 +1,13 @@
+package app.emporioDaVila;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmporioDaVilaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmporioDaVilaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/emporioDaVila/EmporioDaVilaApplicationTests.java
@@ -0,0 +1,13 @@
+package app.emporioDaVila;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmporioDaVilaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
