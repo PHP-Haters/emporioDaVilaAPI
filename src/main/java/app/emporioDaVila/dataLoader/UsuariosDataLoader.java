be775859f6be1376c167fbb471a8eb030b4d1283
Gabriel Castro <gabrielcastro.developer@gmail.com>
2025-11-28 09:22:52 -0300

    usuarioDataLoader arrumado considerando criptografia na service

--- src/main/java/app/emporioDaVila/dataLoader/UsuariosDataLoader.java
+++ src/main/java/app/emporioDaVila/dataLoader/UsuariosDataLoader.java
@@ -13,11 +13,11 @@ public class UsuariosDataLoader {
     CommandLineRunner initUsuariosDatabase(UsuarioService usuarioService) {
         return args -> {
             // senha = admin
-            Usuario admin = new Usuario("Admin", "$2a$12$wKv.F56KNmOWsY9ohozbuuMUey6s5TMBB1A7zlgeBBXu9Om0X0XAu", "45900000000", "admin@gmail.com", "ROLE_ADMIN");
+            Usuario admin = new Usuario("Admin", "admin", "45900000000", "admin@gmail.com", "ROLE_ADMIN");
             usuarioService.saveUsuario(admin);
 
             // senha = johnDoe
-            Usuario tester = new Usuario("John Doe", "$2a$12$rN4kDek4S3u9bcv6q1ML9.jsAILz6Wx4Hdu5kwZ3Ld3Mi2Becx1ry", "45900000000", "johnDoe@gmail.com", "ROLE_USER");
+            Usuario tester = new Usuario("John Doe", "johnDoe", "45900000000", "johnDoe@gmail.com", "ROLE_USER");
             usuarioService.saveUsuario(tester);
         };
     }
